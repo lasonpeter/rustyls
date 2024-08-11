8dcf2c37db3c5797ffb880091764b8e65a1e131d
xenu <xenucode@gmail.com>
2024-08-11 04:09:27 +0200

    Project update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rustyls
-A rusty equivalent to linux's  'ls' command
+### A simple rusty equivalent to linux's 'ls' command
+Created for purpose of learning Rust programming language
+## Usage
+Just execute the binary like this on Linux: `./rustyls` and you will get a list of all directories and files with
+color coding
+![rustyls.png](readme/rustyls.png)
--- src/main.rs
+++ src/main.rs
@@ -23,30 +23,7 @@ fn main() {
         if x.eq("-a"){
             show_hidden = true;
         }
-        /*
-        if match fs::metadata(x) {
-            Ok(metadata) => {
-               if metadata.is_dir()
-                {
-                    true
-                }
-                else {
-                    panic()
-                }
-            }
-            Err(err) => {
-                match err.kind() {
-                    ErrorKind::NotFound => {panic!("Path not found")}
-                    ErrorKind::PermissionDenied => {panic!("Permission denied")}
-                    ErrorKind::Other => {panic!("Something went wrong")}
-                }
-            }
-        }
-        */
-        {
-
-        }
     }
     ls::list_files_in_current_dir(show_hidden, path);
-
+    print!("\n");
 }
